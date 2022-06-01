07e9f9e728bfd39d49835d8e99e1373c8f1e4ede
Dakatalol <dakars@mail.bg>
2022-06-01 23:45:27 +0300

    removing comments

--- Tests/login_test.py
+++ Tests/login_test.py
@@ -3,7 +3,6 @@ from Pages.inventory.inventory_page import InventoryPage
 
 
 def test_login():
-    # User has logged into the Prelab application
     LoginPage.set_user_name("standard_user")
     LoginPage.set_password("secret_sauce")
     LoginPage.click_login_button()
